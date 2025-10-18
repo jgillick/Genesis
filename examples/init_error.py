7d1e69364130915f3b350bc698e6dc28ddaded09
Jeremy <j_gillick@yahoo.com>
2025-10-18 10:19:15 -0700

    Minimal example for init error

---
+++ examples/init_error.py
@@ -0,0 +1,23 @@
+
+
+import argparse
+import genesis as gs
+from genesis.engine.entities import RigidEntity
+
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument("-d", "--device", type=str, default="gpu")
+args = parser.parse_args()
+
+def main():
+    # Initialize Genesis
+    backend = gs.gpu
+    if args.device == "cpu":
+        backend = gs.cpu
+    gs.init(logging_level="warning", backend=backend, performance_mode=True)
+
+    # Do something here
+    while True:
+        pass
+
+if __name__ == "__main__":
+    main()
